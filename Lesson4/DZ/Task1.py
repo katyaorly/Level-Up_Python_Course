4e5417af5d1e3c4e7ab73275dc121f5d1ef3e7e8
Екатерина <katusha_orlova@mail.ru>
2022-07-28 19:12:52 +0700

    python

--- Lesson4/DZ/Task1.py
+++ Lesson4/DZ/Task1.py
@@ -1,5 +1,5 @@
 # Вывод действительного корня квадратного уравнения
-from func_quad_eq import count_quad_eq
+from Lesson4.DZ.func_quad_eq import count_quad_eq
 
 a, b, c = map(int, input("Введите коэффициенты для квадратного уравнения: ").split())
 print(count_quad_eq(a, b, c))
