d2e12b730ab41ab543eed31cead1a61dcfedfba3
Екатерина <katusha_orlova@mail.ru>
2022-07-29 20:18:01 +0700

    2 в 10

--- Lesson8/Decimal.py
+++ Lesson8/Decimal.py
@@ -1,9 +1,17 @@
 #  Перевод числа из двоичной системы в десятичную
 
+# a = input("Введите число в двоичной системе: ")
+# b = 0
+# ext = 0
+# for i in reversed(a):
+#     b += (2 ** ext) * int(i)
+#     ext += 1
+# print("Число в десятичной системе: ", b)
+
+#  Вариант 2
+
 a = input("Введите число в двоичной системе: ")
-b = 0
-ext = 0
-for i in reversed(a):
-    b += (2 ** ext) * int(i)
-    ext += 1
-print("Число в десятичной системе: ", b)
+decimal = 0
+for i in a:
+    decimal = decimal * 2 + int(i)
+print("Число в десятичной системе: ", decimal)
